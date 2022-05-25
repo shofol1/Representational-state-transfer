ba2c55627e4a71d1557bc2d11edccc114f334fb2
unknown <18103156@iubat.edu>
2022-05-25 14:22:45 +0600

    get all articles

--- app.js
+++ app.js
@@ -3,19 +3,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 
-//TODO
+//connection db
+const uri = `mongodb://localhost:${process.env.MONGOPORT}/wikiDB`;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+});
+console.log(uri);
+//creating schema
+const articleSchema = mongoose.Schema({
+  tile: String,
+  content: String,
+});
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+//creating model
+const Article = mongoose.model("articles", articleSchema);
+
+//TODO
+app.get("/articles", (req, res) => {
+  Article.find((err, foundArticles) => {
+    if (!err) {
+      console.log(foundArticles);
+    }
+  });
+});
+
+app.listen(process.env.PORT, function () {
+  console.log("Server started on port ", process.env.PORT);
 });
